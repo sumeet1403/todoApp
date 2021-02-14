c1a89ff5c2451caa915a9377a555f65ec0c9f182
Sumeet Sharma <sumeetxsharma@deloitte.co.uk>
2021-02-14 06:51:42 +0000

    clean up

--- Todo.Test/TodoUnitTest.cs
+++ Todo.Test/TodoUnitTest.cs
@@ -13,22 +13,14 @@ namespace Todo.Test
         private TodoItemsController _todoItemsController;
         private DbContextOptions<TodoContext> ContextOptions { get; }
 
-        //public TodoUnitTest(TodoContext context)
-        //{
-        //    _context = context;
-        //    _todoItemsController = new TodoItemsController(context);
-        //}
-
+        
         [TestInitialize]
         public void Initalize()
         {
-            //_context = new TodoContext(ContextOptions);
-
             var options = new DbContextOptionsBuilder<TodoContext>()
                             .UseInMemoryDatabase(databaseName: "testTodoDB")
                             .Options;
             var dbContext = new TodoContext(options);
-            //var mockSet = new Mock<TodoContext>();
             _todoItemsController = new TodoItemsController(dbContext);
         }
 
@@ -44,8 +36,6 @@ namespace Todo.Test
 
             var toDoItemCreatedReponse = _todoItemsController.PostTodoItem(toDoItem).Result;
             var response = Xunit.Assert.IsType<ActionResult<TodoItem>>(toDoItemCreatedReponse);
-            //var code =response.Result.StatusCode;
-            //Assert.IsInstanceOfType(toDoItemCreatedReponse, typeof(CreatedAtActionResult));
             Assert.AreEqual(201, ((Microsoft.AspNetCore.Mvc.ObjectResult)response.Result).StatusCode.Value);
         }
 
